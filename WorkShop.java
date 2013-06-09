54e00d0e753b5adaa469e384d2932f45ca9f501b
DAnagnostopoulou <dion.anagn@gmail.com>
2013-06-09 18:40:51 +0300

    Update WorkShop.java
    Updated based on mail comments

--- WorkShop.java
+++ WorkShop.java
@@ -1,16 +1,16 @@
+package javaWorkShop;
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Formatter;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
 
-public class WorkShop {
-  
-	private Set<String> hashSet1 = new HashSet<String>(); 
+public class WorkShop implements IWorkShop{
+	
+	private Set<String> treeSet1 = new TreeSet<String>(); 
 	private Set<String> hashSet2 = new HashSet<String>(); 
 	private Formatter output;
 	private String file1;
@@ -33,7 +33,7 @@ public class WorkShop {
 			while (input.hasNext())
 			{
 				String word = input.nextLine();
-				hashSet1.add(word);
+				treeSet1.add(word);
 			}
 			
 			input = new Scanner(new File(file2));
@@ -45,34 +45,20 @@ public class WorkShop {
 		}
 		catch (FileNotFoundException e)
 		{
-			System.out.println("File not found!");
+			e.printStackTrace();
 			output.close();
 			System.exit(1);
-		}
+		}		
 	}
 	
-	void compareContents()
-	{
-		ArrayList<String> array1 = new ArrayList<String>(new TreeSet<String>(hashSet1));
-		ArrayList<String> array2 = new ArrayList<String>(new TreeSet<String>(hashSet2));
-
-		for (String word : array1)
+	public void compareContents()
+	{		
+		for (String word : treeSet1)
 		{
-			if (Collections.binarySearch(array2, word) >=0 ) 
-				output.format("%s\n", word);
+				if (hashSet2.contains(word))
+					output.format("%s\n", word);
 		}
 		output.close();
-	}
-	
-	public static void main(String[] args) {
-		if (args.length!=3)
-		{
-			System.out.println("You should provide 3 args!");
-			System.exit(1);
-		}
-		WorkShop test = new WorkShop(args);
-		test.processFiles();
-		test.compareContents();
 	}
 }
 
